81996c8147eba3c9ee038a63cd614a1c47fe70c6
Gunjankher <gkhercode@gmail.com>
2024-06-02 15:45:55 +0530

    uplaod file on cloudinary

--- src/utilis/cloudinary.js
+++ src/utilis/cloudinary.js
@@ -1,4 +1,5 @@
   import {v2 as cloudinary} from 'cloudinary'
+import { log } from 'console';
   import fs from 'fs'
 
           
@@ -7,3 +8,29 @@
     api_key: process.env.CLOUDINARY_API_KEY, 
     api_secret:process.env.CLOUDINARY_API_SECRET
   });
+
+
+
+  const uploadOnCloudinary = async(localFilePath)=>{
+try {
+    if(!localFilePath) return null
+    // upload on cloudnary
+    if(localFilePath){
+    const response = await cloudinary.uploader.upload(localFilePath,{
+        resource_type :'auto'
+    })
+//file has been uploded sucessfully 
+console.log("File has been Uploaded on Cloudinary", response.url);
+
+return response
+
+    }
+    
+} catch (error) {
+    fs.unlinkSync(localFilePath) //remove the filepath if the upload failed
+    return null
+}
+
+  }
+
+  export {uploadOnCloudinary}

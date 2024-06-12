48904a69ece4290980c82800be031daf9863b809
Gunjankher <gkhercode@gmail.com>
2024-06-12 15:05:54 +0530

    mongodb pipeline

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -434,15 +434,52 @@ $addFields :{
 
  channelIsSubscribedCount :{
   $size : "$subscribedTo"
- }
+ },
+ 
+ // checck if user is subscribe or not and give to the front end
+
+isSubscribed :{
+  $cond :{
+    if : {$in:[req.user?._id, "$subscribers.subscriber"]},
+    then : true,
+    else :false
+  }
+}
+
 
 }
 
+},
+
+// fifth pipeline 
+{
+  $project: {
+      fullName: 1,
+      username: 1,
+      subscribersCount: 1,
+      channelIsSubscribedCount: 1,
+      isSubscribed: 1,
+      avatar: 1,
+      coverImage: 1,
+      email: 1
+
+  }
 }
 
 
 ])
 
+
+
+if (!channel?.length) {
+  throw new ApiError(404, "channel does not exists")
+}
+
+return res
+.status(200)
+.json(
+  new ApiResponse(200, channel[0], "User channel fetched successfully")
+)
 
 
 })

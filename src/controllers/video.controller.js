f333e1ae1e67342b3e343019ba9d907143148fa8
Gunjankher <gkhercode@gmail.com>
2024-08-02 17:03:56 +0530

    video conrtrollers

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,68 +1,79 @@
-import { Video } from "../models/video.model";
-import { ApiError } from "../utilis/ApiError";
-import { ApiResponse } from "../utilis/ApiResponse";
-import { asyncHandlar } from "../utilis/asyncHandlar";
-import { uploadOnCloudinary } from "../utilis/cloudinary";
+import { Video } from "../models/video.model.js";
+import { ApiError } from "../utilis/ApiError.js";
+import { ApiResponse } from "../utilis/ApiResponse.js";
+import { asyncHandlar } from "../utilis/asyncHandlar.js";
+import { uploadOnCloudinary } from "../utilis/cloudinary.js";
 
 
 
 
 const publishVideo = asyncHandlar(async(req,res)=>{
 
-const {title,description} = req.body
-
-if([title,description].some((field)=>field.trim()===0)){
-    throw new ApiError(401,`All Fields are Required`)
+try {
+    const {title,description} = req.body
+    
+    if([title,description].some((field)=>field.trim()===0)){
+        throw new ApiError(401,`All Fields are Required`)
+    }
+    
+     const videoFileLocalPath = req.files?.videoFile[0].path
+    const thumbnailLocalPath = req.files?.thumbnail[0].path
+    
+    if(!videoFileLocalPath){
+    throw new ApiError(400, `videoLocalpath is required`)
+    }
+    if(!thumbnailLocalPath){
+    throw new ApiError(400, `thumbnailLocalPath is required`)
+    }
+    
+    
+    
+      const videoFile = await uploadOnCloudinary(videoFileLocalPath)
+      const thumbnail = await uploadOnCloudinary(thumbnailLocalPath)
+    
+      if(!videoFile){
+        throw new ApiError(400, `videoFile is required`)
+      }
+      if(!thumbnail){
+        throw new ApiError(400, ` thumbnail is required`)
+      }
+    
+    
+      const video = await Video.create({
+        title,
+        description,
+        duration: videoFile.duration,
+        videoFile: {
+            url: videoFile.url,
+            public_id: videoFile.public_id
+        },
+        thumbnail: {
+            url: thumbnail.url,
+            public_id: thumbnail.public_id
+        },
+        owner: req.user?._id,
+        isPublished: false
+    });
+    
+     const videoUploaded = await Video.findById(video._id)
+    
+    
+    if(!videoUploaded){
+        throw new ApiError(400, `Uploading failed please try again`)
+    }
+    
+    return res
+            .status(200)
+            .json(new ApiResponse(200, video, "Video uploaded successfully"));
+    
+} catch (error) {
+    console.error(`Error while Publishing video`,error);
+    
 }
-
- const videoFileLocalPath = req.files?.videoFile[0].path
-const thumbnailLocalPath = req.files?.thumbnail[0].path
-
-if(!videoFileLocalPath){
-throw new ApiError(400, `videoLocalpath is required`)
-}
-if(!thumbnailLocalPath){
-throw new ApiError(400, `thumbnailLocalPath is required`)
-}
-
-
-
-  const videoFile = await uploadOnCloudinary(videoFileLocalPath)
-  const thumbnail = await uploadOnCloudinary(thumbnailLocalPath)
-
-  if(!videoFile){
-    throw new ApiError(400, `videoFile is required`)
-  }
-  if(!thumbnail){
-    throw new ApiError(400, ` thumbnail is required`)
-  }
-
-
- const video = await Video.create({
-    title,
-    description,
-    duration :videoFile.duration,
-    videoFile :{
-        url :videoFile.url,
-        public_id : videoFile.public_id
-    },
-    thumbnail :{
-        url :thumbnail.url,
-        public_id :thumbnail.public_id
-    },
-    owner : req.user?._id,
-    isPublished : false
 })
 
- const videoUploaded = await Video.findById(video._id)
 
 
-if(!videoUploaded){
-    throw new ApiError(400, `Uploading failed please try again`)
+export {
+    publishVideo,
 }
-
-return res
-        .status(200)
-        .json(new ApiResponse(200, video, "Video uploaded successfully"));
-
-})
--- src/routes/video.route.js
+++ src/routes/video.route.js
@@ -1,9 +1,29 @@
 import {Router} from 'express'
+import { publishVideo } from '../controllers/video.controller.js'
+import { verifyJWT } from '../middlewares/auth.middleware.js'
+import { upload } from '../middlewares/multer.middleware.js'
+
 
 
 
 const router = Router()
 
+router.route('/new').post(
+upload.fields([
+    {
+        name :"videoFile",
+        maxCount :1
+    },
+{
+    name :"thumbnail",
+    maxCount :"1"
+}
+
+]),
+
+publishVideo
+
+)
 
 
 

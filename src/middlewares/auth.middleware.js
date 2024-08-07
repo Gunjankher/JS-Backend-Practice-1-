769a97c88540b9832df7b9a3e6809fe6aa654bd6
Gunjankher <gkhercode@gmail.com>
2024-08-07 14:38:31 +0530

    video controllers

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -5,21 +5,23 @@ import { User } from "../models/user.model.js";
 
 
 
-export const verifyJWT = asyncHandlar(async(req,res,next)=>{
+export const verifyJWT = asyncHandlar(async(req,_,next)=>{
 
 try {
      const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer","")
     
+    // console.log(token);
     
      if(!token){
         throw new ApiError(401, "Unatuhorized Request")
      }
     
       const decodedToken = jwt.verify(token , process.env.ACCESS_TOKEN_SECRET)
-    
+      // console.log('Decoded Token:', decodedToken); // Log the decoded token
       const user = await User.findById(decodedToken?._id).select(
         "-password -refreshToken"
       )
+      // console.log('User:', user); // Log the user
     
       if(!user){
         throw new ApiError(401, "Invalid Access Token")
@@ -27,6 +29,8 @@ try {
     
       req.user = user
       next()
+      // console.log(req.user);
+      
     
 } catch (error) {
     throw new ApiError(401 , error.message || "Inavalid Access Token ")
--- src/routes/video.route.js
+++ src/routes/video.route.js
@@ -1,34 +1,39 @@
-import {Router} from 'express'
-import { publishVideo, updateVideo } from '../controllers/video.controller.js'
-import { verifyJWT } from '../middlewares/auth.middleware.js'
-import { upload } from '../middlewares/multer.middleware.js'
+import { Router } from "express";
+import { deleteVideo, publishVideo, updateVideo } from "../controllers/video.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
+const router = Router();
 
-
-
-const router = Router()
-
-router.route('/new').post(
-upload.fields([
+router.route("/new").post(
     verifyJWT,
+  upload.fields([
     {
-        name :"videoFile",
-        maxCount :1
+      name: "videoFile",
+      maxCount: 1,
     },
-{
-    name :"thumbnail",
-    maxCount :"1"
-}
+    {
+      name: "thumbnail",
+      maxCount: "1",
+    },
+  ]),
 
-]),
-
-publishVideo
-
-)
-router.route(`/v/:videoId`)
-.patch(verifyJWT,upload.single("thumbnail"),updateVideo)
+  publishVideo,
+);
 
 
+router.route('/v/:videoId')
+  .patch(
+    verifyJWT,               
+    upload.single('thumbnail'), 
+    updateVideo
+  )
+  .delete(
+    verifyJWT,               
+    upload.none(),
+    deleteVideo
+  );
 
 
-export default router
+
+export default router;

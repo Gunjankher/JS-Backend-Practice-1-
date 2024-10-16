619aed343f162d63aeedbd82b3bce64547d34428
Gunjankher <gkhercode@gmail.com>
2024-10-16 10:48:25 +0530

    subscription controller

---
+++ src/controllers/subscription.controller.js
@@ -0,0 +1,13 @@
+import { ApiError } from "../utilis/ApiError.js";
+import { ApiResponse } from "../utilis/ApiResponse";
+import { asyncHandlar } from "../utilis/asyncHandlar.js";
+
+
+const toggleSubscription = asyncHandlar(async(req,res)=>{
+    // get the channel id for params
+    // check object id 
+    // find the channel by findone  subscriber , channel 
+    // find find by id and delete subsciebed 
+    // return res
+    
+})

8261fb336778eeb78980b79540a0c58ae1b182ef
Gunjankher <gkhercode@gmail.com>
2024-10-21 10:32:24 +0530

    playlist controllers

--- src/app.js
+++ src/app.js
@@ -26,6 +26,7 @@ import videoRouter from './routes/video.route.js'
 import likeRouter from './routes/like.router.js'
 import commentRouter from'./routes/comment.router.js'
 import tweetRouter from './routes/tweet.router.js'
+import playlistRouter from './routes/playlist.router.js'
  
 
 // Route Declaraion 
@@ -35,6 +36,7 @@ app.use("/api/v1/video", videoRouter)
 app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/comment", commentRouter)
 app.use("/api/v1/tweet", tweetRouter)
+app.use("/api/v1/playlist",playlistRouter)
 
 
 
